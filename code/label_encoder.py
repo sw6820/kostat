4311ab4b5f13e2873c332a4481845c9be1cdd15c
Dong-geon <jdg4661@gmail.com>
2022-04-12 21:10:35 +0900

    remove kobert and kodistilbert, fix data file location label_encoder.py

--- code/label_encoder.py
+++ code/label_encoder.py
@@ -2,7 +2,7 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 
 def get_label_encoder():
-    ctd = pd.read_csv("data/class_table_chunk_kor.csv")
+    ctd = pd.read_csv("../data/class_table_chunk_kor.csv")
     ctd.loc[:,"code"] = ctd.loc[:,"code"].astype(str)
 
     label_encoder = LabelEncoder()
--- code/model.py
+++ code/model.py
@@ -1,6 +1,5 @@
 from transformers import AutoModel, AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
 from logger import get_logger
-from tokenization_kobert import KoBertTokenizer # 같은 위치에 tokenization_kobert.py 
 
 class Model:
     """
@@ -27,10 +26,7 @@ class Model:
 
     def get_tokenizer(self):
         try:
-            if self.model_name == "monologg/kobert" or self.model_name == "monologg/distilkobert":
-                tokenizer = KoBertTokenizer.from_pretrained("monologg/kobert") 
-            else:
-                tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+            tokenizer = AutoTokenizer.from_pretrained(self.model_name)
             self.logger.info(f"Success Loading tokenizer: {self.model_name}")
         except:
             self.logger.exception(f"Fail Loading tokenizer: {self.model_name}")
